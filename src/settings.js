b5e2409a857e68817dbe9ef9edfe451e480e0629
GoodOldNin <71062853+GoodOldNin@users.noreply.github.com>
2022-06-28 19:28:32 +0200

    autodeploy3

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
-export const loginUrl =
-  "http://localhost:8080/24TimersEksamen_war_exploded/api/login";
+export const loginUrl = "https://cloudcastle.dk/24TimersEksamen/api/login";
+// http://localhost:8080/24TimersEksamen_war_exploded/api/login
 // example export const signUpUrl = "https://test.singah.dk/SP/api/signup";
 // example export const signUpUrl = "https://test.singah.dk/SP/api/signup";
 // example export const signUpUrl = "https://test.singah.dk/SP/api/signup";
